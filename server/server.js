91b285682a45ee2ed5a2efb868acec51e0189c84
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2022-02-14 09:18:58 -0800

    connect to the database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const app = require("./app");
 
+dotenv.config();
+
+// =========== Connect to the database ===========
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_KEY}@cluster0.olp8q.mongodb.net/${process.env.DB_NAME}`
+  )
+  .then((res) => {
+    console.log("Successfully connected to the database.");
+  })
+  .catch((err) => console.error(err));
+
+// =========== Set server to listen ===========
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
