5041a79209c52846d5a93a6ed858c521298cc330
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2022-02-14 09:31:37 -0800

    setup routers for the api post and get requests

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const router = require("./routers/duckDataRouter");
+
 // =========== Start express application ===========
 const app = express();
 
@@ -10,5 +12,8 @@ app.use(express.json());
 // =========== Implement CORS ===========
 // Allowing the frontend application to access the backend.
 app.use(cors({ origin: "*" }));
+
+// =========== Routes ===========
+app.use("/duckdata", router);
 
 module.exports = app;
